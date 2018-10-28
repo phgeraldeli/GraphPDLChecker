bad89bad69fd4589ef6e4f782a7b4fe2aa7e9f11
unknown <phgeraldeli@id.uff.br>
2018-10-27 23:19:44 -0300

    Correção do projeto de acordo com a definição, remoção da valoração

--- GraphCheck.rkt
+++ GraphCheck.rkt
@@ -1,18 +1,18 @@
 #lang racket
 
 
-(struct graph-body (W R V)) ; Estrutura do grafo será vertices relações valoração
-(define all-graph ( graph-body (list 'A 'B) #| Vertices |# (list (list 'alfa(list 'a 'b) 'alfa (list 'b 'a)) 'beta (list 'a 'b))#| Relações alfa e beta |# (list (list 'psi 'omega) (list 'psi)) #|  Valorações em A e B|#))
+(struct graph-body (W R)) ; Estrutura do grafo será vertices relações valoração
+(define all-graph ( graph-body (list 'A 'B) #| Vertices |# (list (list '(A B alfa) '(B A alfa))))) #| Relações alfa e beta |#
   
 (struct PDL (program))
-(define allprog ( PDL (list '[alfa]'psi'[alfa]'psi?'omega)))
+(define allprog ( PDL (list alfa beta alfa)))
 
 
 
 #| Duvidas:
-    Estrutura do grafo precisa da valoração?
-    Estrutura do PDL será [alfa]'psi ou só alfa;psi?;beta
-    Como checar se existe uma arestra dentro da lista
-    input do arquivo para dentro do struct
-    Como utilizar o charactere especial ; sem virar comentário
+    Estrutura do grafo precisa da valoração? Não
+    Estrutura do PDL será [alfa]'psi ou só alfa;psi?;beta? alfa;psi;beta
+    Como checar se existe uma arestra dentro da lista? car e cdr
+    input do arquivo para dentro do struct? 
+    Como utilizar o charactere especial ; sem virar comentário? 
 |#
